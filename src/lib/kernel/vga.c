02c8a37b1c12fc4efe8a4ca8b4e10d9fe4634a35
Jose <48543854+LavamasterYT@users.noreply.github.com>
2024-10-16 00:50:03 -0500

    Fixed merge

--- src/lib/kernel/vga.c
+++ src/lib/kernel/vga.c
@@ -1,4 +1,3 @@
-
 #include "vga.h"
 
 #include <stdint.h>
@@ -43,15 +42,21 @@ void vga_clear()
 
 void vga_write_str_at(char* input, uint8_t x, uint8_t y)
 {
+    if (input == NULL)
+    {
+        vga_set_cursor_position(x, y);
+        return;
+    }
+
     volatile vga_cell* video_mem = (vga_cell*)0xB8000;
 
     char* ptr = input;
     uint8_t offset = 0;
 
-    while (*ptr != '\0')
+    while (*ptr)
     {
         video_mem[(x + offset) + 80 * y].ascii = *ptr;
-        video_mem[(x + offset) + 80 * y].color = 0x08;
+        video_mem[(x + offset) + 80 * y].color = 0x0F;
 
         offset++;
         ptr++;
